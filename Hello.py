b7c81b57ccae3071f0f70a8fb5a2612c4edde9b4
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-12-10 14:40:26 +0000

    first update

--- Hello.py
+++ Hello.py
@@ -1,51 +1,17 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+import mag4 as mg
+import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
-    run()
+st.title('Hello Katharina')
+
+df = mg.get_data('Banda Arc')
+
+el1 = st.selectbox('sel x', df.columns.tolist()[27:])
+el2 = st.selectbox('sel y', df.columns.tolist()[27:])
+fig, ax = plt.subplots()
+ax.scatter(df[el1], df[el2])
+st.pyplot(fig)
+
+
+st.dataframe(mg.get_data('abminsym'))
